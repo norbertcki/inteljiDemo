9a1a303b430399e9387251e0e3e993706600f463
norbertcki.com <jak677@wp.pl>
2018-04-08 17:47:26 +0100

    pierwszy comit po orginalnym

--- src/com/norberto/Main.java
+++ src/com/norberto/Main.java
@@ -3,6 +3,7 @@ package com.norberto;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.print("pierwszy po initial-orginalnym comit");
 	// write your code here
     }
 }
